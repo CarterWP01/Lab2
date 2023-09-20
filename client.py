fe8ed55fb53b46df98128d8954914e5b52fe6dfb
CarterWP01 <zekewp101@gmail.com>
2023-09-19 19:43:37 -0600

    commit

--- client.py
+++ client.py
@@ -5,7 +5,7 @@ BytesToRead = 4096
 def get(host, port):
     request = b"GET / HTTP/1.1\nHOST: " + host.encode('utf-8') + b"\n\n"
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host,port))
+    s.connect((host, port))
     s.send(request)
     s.shutdown(socket.SHUT_WR)
     result = s.recv(BytesToRead)
@@ -15,5 +15,5 @@ def get(host, port):
     s.close()
 
 
-#get("www.google.com",80)
+#get("www.google.com", 80)
 get("127.0.0.1", 8080)
--- echo_server.py
+++ echo_server.py
@@ -1,10 +1,12 @@
 import socket
 from threading import Thread
+
 BytesToRead = 4096
 HOST = "127.0.0.1"
 PORT = 8080
 
-def handleConnection(conn,addr):
+
+def handleConnection(conn, addr):
     with conn:
         print(f"connected by {addr}")
         while True:
@@ -14,15 +16,16 @@ def handleConnection(conn,addr):
             print(data)
             conn.sendall(data)
 
+
 def startServer():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((HOST, PORT))
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen()
         while True:
-            conn,addr = s.accept()
+            conn, addr = s.accept()
             handleConnection(conn, addr)
-        
+
 
 def startThreadedServer():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -30,9 +33,10 @@ def startThreadedServer():
         s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.listen(2)
         while True:
-            conn,addr = s.accept()
-            thread = Thread(target = handleConnection, args=(conn, addr))
+            conn, addr = s.accept()
+            thread = Thread(target=handleConnection, args=(conn, addr))
             thread.run()
 
-#startServer()
+
+# startServer()
 startThreadedServer()
